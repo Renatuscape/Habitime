f50a28d50c3026e866651707b8072f4e995d60dd
Renatuscape <rrjonsberg@gmail.com>
2026-03-07 12:48:55 +0100

    Add adventurer template data

---
+++ Assets/Scripts/Data/AdventurerTemplate.cs
@@ -0,0 +1,10 @@
+using System;
+
+[Serializable]
+public class AdventurerTemplate
+{
+    public int id;
+    public string name;
+    public double xpBase; // XP until level 1
+    public double xpCap; // Milliseconds until level 100
+}
