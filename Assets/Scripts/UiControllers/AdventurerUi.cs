690d0fe4025c0253cc4e73054cd57dbd1e2a22ba
Renatuscape <rrjonsberg@gmail.com>
2026-03-07 12:52:03 +0100

    Add clickable logic and animation triggers

--- Assets/Scripts/UiControllers/AdventurerUi.cs
+++ Assets/Scripts/UiControllers/AdventurerUi.cs
@@ -3,26 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AdventurerUi : MonoBehaviour
 {
     public TextMeshProUGUI levelText;
+    public Button btAdventurer;
     bool isActive;
 
     private void OnEnable()
     {
-        StartCoroutine(AdventureChecker());
+            StartCoroutine(AdventureChecker());
+    }
+
+    public void ClickAdventurer()
+    {
+        AnimatorTool.JumpObject(btAdventurer.gameObject, 5, 0.2f);
     }
 
     IEnumerator AdventureChecker()
     {
         while (true)
         {
-
-            if (!isActive && MainController.activeWatch != null && levelText != null)
+            if (DataTools.playerData != null && DataTools.playerData.disableAdventureMode)
+            {
+                gameObject.SetActive(false);
+            }
+            else if (!isActive && DataTools.activeClock != null && levelText != null)
             {
                 isActive = true;
                 StartCoroutine(UpdateLoop());
+
+                if (btAdventurer != null)
+                {
+                    btAdventurer.onClick.RemoveAllListeners();
+                    btAdventurer.onClick.AddListener(() => ClickAdventurer());
+                }
+
                 break;
             }
             else if (!isActive)
@@ -30,15 +47,21 @@ public class AdventurerUi : MonoBehaviour
                 isActive = false;
             }
 
-            yield return new WaitForSeconds(1.00f);
+            yield return new WaitForSeconds(0.01f);
         }
     }
 
     IEnumerator UpdateLoop()
     {
+        string prevText = levelText.text;
         while (true)
         {
-            levelText.text = "Lv. " + AdventureTools.GetLevel(MainController.activeWatch.GetTime());
+            levelText.text = "Lv. " + AdventureTools.GetLevel(ClockTools.GetTime(DataTools.activeClock));
+            if (levelText.text != prevText)
+            {
+                AnimatorTool.JumpObject(levelText.gameObject, 30, 0.1f);
+                prevText = levelText.text;
+            }
             yield return new WaitForSeconds(1.00f);
         }
     }
