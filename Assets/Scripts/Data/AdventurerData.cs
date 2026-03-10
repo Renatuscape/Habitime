30c4beb2b007e7d83739c40089b3a3965db25645
Renatuscape <rrjonsberg@gmail.com>
2026-03-11 00:58:03 +0100

    Add save data for max level and end/start time

--- Assets/Scripts/Data/AdventurerData.cs
+++ Assets/Scripts/Data/AdventurerData.cs
@@ -4,9 +4,12 @@ using System;
 public class AdventurerData
 {
     public int clockId; // Relevant clock
-    public int templateId;
+    public AdventurerTemplate template;
     public string name;
     public bool isDead;
+    public bool isMaxed;
     public int bonusXP;
     public int prestige; // Times advanced
+    public long startTimestamp;
+    public long endTimestamp;
 }
