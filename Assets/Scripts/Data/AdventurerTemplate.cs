e53a5f76566ee0562ab513d02b07ebecab84a213
Renatuscape <rrjonsberg@gmail.com>
2026-03-11 00:58:16 +0100

    Add max level variable

--- Assets/Scripts/Data/AdventurerTemplate.cs
+++ Assets/Scripts/Data/AdventurerTemplate.cs
@@ -7,4 +7,5 @@ public class AdventurerTemplate
     public string name;
     public double xpBase; // XP until level 1
     public double xpCap; // Milliseconds until level 100
+    public double maxLevel = 100;
 }
