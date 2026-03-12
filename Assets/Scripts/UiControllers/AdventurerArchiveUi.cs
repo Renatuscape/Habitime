a80e60759f86ebaabf0ad2822c1d4303dc2c1077
Renatuscape <silarcta@gmail.com>
2026-03-12 11:44:27 +0100

    Add additional checks to prevent while loop crash

--- Assets/Scripts/UiControllers/AdventurerArchiveUi.cs
+++ Assets/Scripts/UiControllers/AdventurerArchiveUi.cs
@@ -13,20 +13,28 @@ public class AdventurerArchiveUi : MonoBehaviour
 
     public void Initialise(ClockData clock)
     {
+        int totalPrinted = 0;
         gameObject.SetActive(true);
 
         List<AdventurerData> retiredAdventurers = clock.adventurers.Where((a) => !a.isDead && a.id != clock.activeAdventurer.id).ToList();
 
-        var leftovers = startBlock.Initialise(retiredAdventurers);
+        var leftovers = startBlock.Initialise(retiredAdventurers, out totalPrinted);
 
-        while (leftovers.Count > 0)
+        int i = leftovers ?? leftovers.Count : 0; // blocks may have more or less than 3 sprite containers in the future
+
+        while (i > 0  && totalPrinted < retiredAdventurers.Count)
         {
             var prefab = Instantiate(archiveBlockPrefab, archiveBlockContainer);
 
             var archiveBlock = prefab.GetComponent<ArchiveBlock>();
             prefabs.Add(archiveBlock);
 
-            leftovers = archiveBlock.Initialise(retiredAdventurers);
+            leftovers = archiveBlock.Initialise(retiredAdventurers, out var printed);
+            totalPrinted += printed;
+
+            i = leftovers ?? leftovers.Count : 0;
+
+            Debug.Log("Leftover adventurers after printing block: " + i + ". Total printed: " + totalPrinted);
         }
     }
 
